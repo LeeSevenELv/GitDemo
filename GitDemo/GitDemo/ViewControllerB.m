46d920272d8221c05f9a9b71ad4ad48029b10338
leemf <qianfeng@SevenElv.local>
2015-11-11 15:51:13 +0800

    B commit change

---
+++ GitDemo/GitDemo/ViewControllerB.m
@@ -0,0 +1,38 @@
+//
+//  ViewControllerB.m
+//  GitDemo
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright (c) 2015年 Lee. All rights reserved.
+//
+
+#import "ViewControllerB.h"
+
+@interface ViewControllerB ()
+
+@end
+
+@implementation ViewControllerB
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
